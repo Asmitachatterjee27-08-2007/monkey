056d8a370a6afd966386f9fd2f00b3a6d6539c58
Asmitachatterjee27-08-2007 <72506203+Asmitachatterjee27-08-2007@users.noreply.github.com>
2020-11-19 21:06:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,89 @@
+var monkey, monkey_running
+var banana, bananaImage, obstacle, obstacleImage
+var FoodGroup, obstacleGroup
+var score;
+
+function preload() {
+
+
+  monkey_running = loadAnimation("sprite_0.png", "sprite_1.png", "sprite_2.png", "sprite_3.png", "sprite_4.png", "sprite_5.png", "sprite_6.png", "sprite_7.png", "sprite_8.png")
+
+  bananaImage = loadImage("banana.png");
+  obstacleImage = loadImage("obstacle.png");
+
+}
+
+
+
+function setup() {
+
+  monkey = createSprite(80, 315, 20, 20);
+  monkey.addAnimation("moving", monkey_running);
+  monkey.scale = 0.1;
+
+  ground = createSprite(400, 350, 900, 10);
+  ground.x = ground.width / 2;
+  ground.velocityX = -4;
+  console.log(ground.x);
+  obstacleGroup = createGroup();
+  FoodGroup = createGroup();
+  var survivalTime = 0;
+}
+
+
+function draw() {
+  background("white");
+
+  if (keyDown("space") && monkey.y >= 300) {
+    monkey.velocityY = -18;
+  }
+  monkey.velocityY = monkey.velocityY + 0.8
+  if (ground.x < 0) {
+    ground.x = ground.width / 2;
+  }
+
+  monkey.collide(ground);
+  spawnFood();
+  spawnObstacle();
+
+  stroke("white");
+  textSize(20);
+  fill("white");
+  text("Score:" + score, 500, 50);
+
+
+  stroke("black");
+  textSize(20);
+  fill("black");
+  survivalTime = Math.ceil(frameCount / frameRate())
+  text("Survival Time:" + survivalTime, 100, 50);
+
+  drawSprites();
+}
+
+function spawnFood() {
+
+  if (frameCount % 80 === 0) {
+    var banana = createSprite(200, 200, 25, 10);
+    banana.addImage(bananaImage);
+    banana.y = Math.round(random(120, 200));
+    banana.velocityX = -5;
+    banana.scale = 0.1;
+    banana.lifetime = 300;
+    FoodGroup.add(banana);
+  }
+
+
+}
+
+function spawnObstacle() {
+  if (frameCount % 300 === 0) {
+    var obstacle = createSprite(200, 200, 25, 10);
+    obstacle.addImage(obstacleImage);
+    obstacle.velocityX = -5;
+    obstacle.scale = 0.1;
+    obstacle.y = 328;
+    obstacle.lifetime = 300;
+    obstacleGroup.add(obstacle);
+  }
+}
